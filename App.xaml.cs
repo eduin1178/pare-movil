5054856a5209c9119d4aac62c52d14d6d6acf5a8
Cristian C. Giraldo <cristiansiempre.net@hotmail.com>
2024-07-11 19:47:20 -0500

    [Add] Creacion del proyecto

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PAREMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PAREMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
